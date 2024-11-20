fb171df8bc1af20038ae8018ebdd85de6958d746
Anjali Singh <122678725+Anjali092903@users.noreply.github.com>
2024-11-20 15:29:02 +0530

    Recursion_Day_2_Sum_of_Array_elements_using_recursion.java

---
+++ Recursion_Day_2_Sum_of_Array_elements_using_recursion.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        int n = sc.nextInt();
+
+
+
+
+
+
+
+      Sum of Array elements using recursion
+Given an array of integers, find sum of array elements using recursion.
+
+Input Format
+Input consists of a 2 lines.
+
+First line contains n.
+
+Second line contains n spaced integers.
+
+Output Format
+Return the sum of array elements.
+
+EXAMPLE 1
+Input:
+
+4
+1 2 3 4
+Output::
+
+10
+EXPLANATION:
+
+1 + 2 + 3 + 4 = 10
+
+EXAMPLE 2
+Input:
+
+3
+1 3 3
+Output::
+
+7
+EXPLANATION:
+
+1 + 3 + 3 = 7.
+
+CONSTRAINTS
+1 <= n <= 10^5
+
+-10^4 <= arr[i] <= 10^4
